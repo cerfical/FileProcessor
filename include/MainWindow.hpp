56d48f7dfea68bc149c2d0b1d08e201a16141def
mitho <@>
2023-09-01 12:54:29 +0500

    Реализована возможность группировки по имени

--- include/MainWindow.hpp
+++ include/MainWindow.hpp
@@ -2,6 +2,7 @@
 #define MAINWINDOW_HPP
 
 #include <QMainWindow>
+#include <QList>
 
 namespace Ui { class MainWindow; }
 
@@ -20,10 +21,41 @@ private slots:
 	void processFile();
 	void saveFile();
 
-	void enableSave();
+	void toggleSaveButton();
 	/** @} */
 
 private:
+	/** @{ */
+	enum GroupPolicy {
+		None,
+		ByType,
+		ByName,
+		ByCreationTime,
+		ByDistance
+	};
+
+	struct ObjectInfo {
+		QString name;
+		QString type;
+		QString x, y;
+		QString time;
+	};
+	/** @} */
+
+	/** @{ */
+	static bool containsCyrillic(const QString& str);
+	/** @} */
+
+	/** @{ */
+	QList<ObjectInfo> parseObjectList();
+
+	void groupByType();
+	void groupByName();
+	void groupByCreationTime();
+	void groupByDistance();
+	/** @} */
+
+	QList<ObjectInfo> m_objects;
 	Ui::MainWindow *m_ui = nullptr;
 };
 
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,16 +1,12 @@
 #include "MainWindow.hpp"
 #include "ui_MainWindow.h"
 
+#include <QRegularExpression>
 #include <QFileDialog>
 #include <QFile>
 
-enum GroupPolicy {
-	None,
-	ByType,
-	ByName,
-	ByCreationTime,
-	ByDistance
-};
+#include <algorithm>
+#include <QMap>
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -34,6 +30,102 @@ MainWindow::~MainWindow() {
 }
 
 
+bool MainWindow::containsCyrillic(const QString& str) {
+	static QRegularExpression regex("[а-яА-Я]");
+	return str.contains(regex);
+}
+
+
+QList<MainWindow::ObjectInfo> MainWindow::parseObjectList() {
+	// получить загруженный ранее список объектов в текстовой форме
+	auto text = m_ui->loadedText->toPlainText();
+	QTextStream in(&text);
+
+	QList<ObjectInfo> objects;
+	ObjectInfo obj;
+
+	QString str;
+	bool groupStarted = false;
+
+	while((in.skipWhiteSpace(), !in.atEnd())) {
+		in >> str;
+
+		// проверить, является ли считанная строка маркером конца группы
+		if(str == "}" && groupStarted) {
+			groupStarted = false;
+			continue;
+		}
+
+		obj.name = str; // сохранить имя объекта
+		in >> str;
+
+		// проверить, является ли считанная строка маркером начала группы
+		if(str == "{" && !groupStarted) {
+			groupStarted = true;
+			continue;
+		}
+		obj.x = str; // сохранить x-координату
+
+		// считать описание объекта и сохранить его в списке
+		in >> obj.y >> obj.type >> obj.time;
+		objects.append(obj);
+	}
+
+	return objects;
+}
+
+
+void MainWindow::groupByType() {
+
+}
+
+void MainWindow::groupByName() {
+	// выполнить группировку по первой букве имени объекта
+	QMap<QString, QList<ObjectInfo>> objectGroups;
+	for(const auto& obj : m_objects) {
+		const auto groupName = obj.name.front();
+		if(containsCyrillic(groupName)) {
+			objectGroups[groupName].append(obj);
+		} else {
+			objectGroups["#"].append(obj);
+		}
+	}
+
+	QString outStr;
+	QTextStream out(&outStr);
+
+	for(auto it = objectGroups.begin(); it != objectGroups.end(); it++) {
+		// при необходимости отсортировать объекты внутри группы
+		auto& objects = it.value();
+		if(m_ui->enableSort->isChecked()) {
+			std::ranges::sort(objects, [](const ObjectInfo& lhs, const ObjectInfo& rhs) {
+				return lhs.name < rhs.name;
+			});
+		}
+
+		// вывести все объекты в группе
+		out << it.key() << " {\n";
+		for(const auto& obj : objects) {
+			out << ' ' << obj.name << ' ';
+			out << obj.x << ' ' << obj.y << ' ';
+			out << obj.type << ' ';
+			out << obj.time << '\n';
+		}
+		out << "}\n";
+	}
+
+	m_ui->processedText->setPlainText(outStr);
+}
+
+void MainWindow::groupByCreationTime() {
+
+}
+
+void MainWindow::groupByDistance() {
+
+}
+
+
 void MainWindow::loadFile() {
 	// запросить у пользователя имя входного файла
 	const auto prevFilename = m_ui->loadedFile->text();
@@ -49,25 +141,43 @@ void MainWindow::loadFile() {
 	// отобразить содержание выбранного файла на экране
 	QFile file(filename);
 	if(file.open(QIODevice::ReadOnly)) {
+		m_objects.clear();
 		m_ui->loadedText->setPlainText(file.readAll());
 		m_ui->loadedFile->setText(filename);
 	}
 }
 
-
 void MainWindow::processFile() {
-	// получить выбранную в настоящее время политику группировки
-	const auto groupPoilcy = m_ui->groupPolicy->currentData().toInt();
-
 	// выполнить группирующее действие и отобразить результат
+	const auto groupPoilcy = m_ui->groupPolicy->currentData().toInt();
+	if(groupPoilcy == None) {
+		m_ui->processedText->setPlainText(m_ui->loadedText->toPlainText());
+		return;
+	}
+
+	if(m_objects.isEmpty()) {
+		m_objects = parseObjectList();
+	}
+
 	switch(groupPoilcy) {
-		case None: {
-			m_ui->processedText->setPlainText(m_ui->loadedText->toPlainText());
+		case ByType: {
+			groupByType();
+			break;
+		}
+		case ByName: {
+			groupByName();
+			break;
+		}
+		case ByCreationTime: {
+			groupByCreationTime();
+			break;
+		}
+		case ByDistance: {
+			groupByDistance();
 			break;
 		}
 	}
 }
-
 
 void MainWindow::saveFile() {
 	// запросить у пользователя имя выходного файла
@@ -90,7 +200,7 @@ void MainWindow::saveFile() {
 }
 
 
-void MainWindow::enableSave() {
+void MainWindow::toggleSaveButton() {
 	if(!m_ui->processedText->document()->isEmpty()) {
 		m_ui->saveButton->setEnabled(true);
 	} else {
